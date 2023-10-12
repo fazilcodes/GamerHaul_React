935814debfc01811fe6305441b55ba3a22f03fc1
Fazil <122675114+fazilcodes@users.noreply.github.com>
2023-10-12 15:54:56 +0530

    Homepage category and contact Done

---
+++ client/src/components/categories/Categories.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './categories.scss'
+import { Link } from 'react-router-dom'
+
+const Categories = () => {
+  return (
+    <div className="categories">
+        <div className="col">
+            <div className="row">
+                <img src="https://wallpaperaccess.com/full/2044892.jpg" alt="" />
+                <button><Link className='link' to='/products/1'>All</Link></button>
+            </div>
+            <div className="row">
+                <img src="https://wallpaperaccess.com/full/2092955.jpg" alt="" />
+                <button><Link className='link' to='/products/1'>Adventure</Link></button>
+            </div>
+        </div>
+
+        <div className="col">
+            <div className="row">
+                <img src="https://wallpaperaccess.com/full/749271.jpg" alt="" />
+                <button><Link className='link' to='/products/1'>Shooting</Link></button>
+            </div>
+        </div>
+
+        <div className="col col-l">
+            <div className="row">
+                <div className="col">
+                    <div className="row">
+                        <img src="https://wallpaperaccess.com/thumb/10275738.jpg" alt="" />
+                        <button><Link className='link' to='/products/1'>Sports</Link></button>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="row">
+                        <img src="https://wallpaperaccess.com/full/13644.jpg" alt="" />
+                        <button><Link className='link' to="/products/1">Racing</Link></button>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <img src="https://wallpaperaccess.com/full/9143.png" alt="" />
+                <button><Link className='link' to="/products/1">Action</Link></button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+
+export default Categories
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import './home.scss'
 import Slider from '../../components/slider/Slider'
 import FeaturedProducts from '../../components/featuredproducts/FeaturedProducts'
+import Categories from '../../components/categories/Categories'
+import Contact from '../../components/contact/Contact'
 
 const Home = () => {
   return (
     <div className='home'>
       <Slider />
       <FeaturedProducts type='Featured' />
+      <Categories />
       <FeaturedProducts  type='Trending' />
+      <Contact />
     </div>
   )
 }
