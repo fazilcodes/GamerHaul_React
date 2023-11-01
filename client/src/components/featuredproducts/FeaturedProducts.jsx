b997314dab312f5a799dae243e0bd93398e9cfd3
Fazil <122675114+fazilcodes@users.noreply.github.com>
2023-11-01 13:19:31 +0530

    api data fetch in home products

--- client/src/components/featuredproducts/FeaturedProducts.jsx
+++ client/src/components/featuredproducts/FeaturedProducts.jsx
@@ -10,7 +10,7 @@ const FeaturedProducts = ({type}) => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const res = await axios.get(`${import.meta.env.VITE_REACT_APP_API_URL}/products?populate=*`, {
+                const res = await axios.get(`${import.meta.env.VITE_REACT_APP_API_URL}/products?populate=*&[filters][type][$eq]=${type}`, {
                     headers: {
                         Authorization: `bearer ${import.meta.env.VITE_REACT_APP_API_TOKEN}`,
                     }
